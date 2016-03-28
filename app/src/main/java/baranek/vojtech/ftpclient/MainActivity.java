b3243bcf12e90a921825e48d631376dbdd377003
孔建华08490 <kjh08490@ly.com>
2016-03-28 19:49:56 +0800

    fix

--- app/src/main/java/baranek/vojtech/ftpclient/MainActivity.java
+++ app/src/main/java/baranek/vojtech/ftpclient/MainActivity.java
@@ -1,27 +1,17 @@
 package baranek.vojtech.ftpclient;
 
-import android.content.ContextWrapper;
-import android.content.Intent;
+import android.app.Activity;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.os.Environment;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
-
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.folderselector.FolderChooserDialog;
-import com.devpaul.filepickerlibrary.FilePickerActivity;
-import com.pixplicity.easyprefs.library.Prefs;
 
 import org.apache.commons.net.ftp.FTPClient;
 import org.apache.commons.net.ftp.FTPFile;
@@ -35,10 +25,9 @@ import java.net.SocketException;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import butterknife.OnClick;
 
 
-public class MainActivity extends AppCompatActivity implements FolderChooserDialog.FolderCallback {
+public class MainActivity extends Activity {
     @Bind(R.id.etCesta)
     TextView etCesta;
     @Bind(R.id.btnPridatAdr)
@@ -46,266 +35,48 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
     @Bind(R.id.btnNahratSoubor)
     Button btnNahratSoubor;
 
-
-    private String FTP_HOST, FTP_USER, FTP_PASS;
-    private String chooserPath = "sdcard/Download";
+    private String FTP_HOST = "27.54.248.35", FTP_USER = "MES", FTP_PASS = "";
     @Bind(R.id.recyclerViewMain)
     RecyclerView recyclerViewMain;
     @Bind(R.id.progress)
     ProgressBar progress;
-    private EditText etNazev, etPw, etAdr;
-
-    private boolean isFirstime = true;
-
 
     private MyRecyclerAdapter adapter;
     private FTPFile[] files;
-    private String strPath = "";
+    private String strPath = "/EWI";
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
-        ThemChanger.onActivityCreateSetTheme(this);
-
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         ButterKnife.bind(this);
-
-        GetLastPreferences();
         recyclerViewMain.setLayoutManager(new LinearLayoutManager(this));
-
-
-        /**
-         * Open configuration dialog if its first app run
-         * */
-
-        if (isFirstime) {
-            ShowDialog();
-            Prefs.putBoolean("prefFirst", false);
-        } else {
-            LoadIntoDirectory(strPath);
-        }
-
-    }
-
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-
-        switch (item.getItemId()) {
-            case R.id.action_settings: {
-                ShowDialog();
-                break;
+        btnPridatAdr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DownloadFile("merge_request.pdf");
             }
-            case R.id.acion_pink: {
-                ThemChanger.changeToTheme(this, ThemChanger.THEME_PINK);
-                break;
-            }
-            case R.id.action_green: {
-                ThemChanger.changeToTheme(this, ThemChanger.THEME_GREEN);
-                break;
-            }
-            case R.id.acion_info: {
-                new MaterialDialog.Builder(this)
-                        .title(getString(R.string.menu_navod))
-                        .content(getString(R.string.usage_info) +
-                                getString(R.string.usage_info2) +
-                                getString(R.string.usage_info3))
-                        .positiveText(R.string.understand)
-                        .show();
-
-                break;
-            }
-            case R.id.action_disconect: {
-                disconnectActions();
-                break;
-            }
-
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
-
-    private void disconnectActions() {
-
-        changeButtonsEnability(false);
-        strPath = "";
-        etCesta.setText(strPath);
-        adapter = new MyRecyclerAdapter(getApplicationContext(), new FTPFile[0], MainActivity.this);
-        recyclerViewMain.setAdapter(adapter);
-
-        Toast.makeText(getApplicationContext(), R.string.dcd, Toast.LENGTH_LONG).show();
+        });
 
     }
 
     private void changeButtonsEnability(boolean val) {
         btnNahratSoubor.setEnabled(val);
         btnPridatAdr.setEnabled(val);
-
     }
 
-    /**
-     * Load last setting preferences
-     */
-    private void GetLastPreferences() {
-        new Prefs.Builder()
-                .setContext(this)
-                .setMode(ContextWrapper.MODE_PRIVATE)
-                .setPrefsName(getPackageName())
-                .setUseDefaultSharedPreference(true)
-                .build();
-
-        FTP_USER = Prefs.getString("prefUSER", "");
-        FTP_PASS = Prefs.getString("prefPass", "");
-        FTP_HOST = Prefs.getString("prefHost", "");
-        isFirstime = Prefs.getBoolean("prefFirst", true);
-
-
-    }
-
-    /**
-     * Open directory method
-     */
-    public void LoadIntoDirectory(String dir) {
-
-        new AssyncFtpTask().execute(dir);
-
-    }
-
-    /**
-     * Delete file method
-     */
-    public void DeleteFileFromFtp(String name) {
-        new AssyncFtpTaskActions().execute("DELETE", name);
-
-
-    }
-
-    /**
-     * Rename file method
-     */
-    public void RenameFileFromFtp(String original, String nove) {
-        new AssyncFtpTaskActions().execute("RENAME", original, nove);
-
-    }
 
     /**
      * Download file method
-     * */
-
-    private String nazevsouborustringos;
-
-    public void DownloadFtpFile(String nazevSouboru) {
-        nazevsouborustringos = nazevSouboru;
-
-    }
-
-    /**
-     *Get directory path from path chooser to for download file
-     * */
-    @Override
-    public void onFolderSelection(File file) {
-        chooserPath = file.getAbsolutePath() + "/" + nazevsouborustringos;
-        new AssyncFtpTaskActions().execute("DOWNLOAD", nazevsouborustringos);
-    }
-
-    /**
-     *Choose file from phone to download
      */
-
-    @OnClick (R.id.btnNahratSoubor)
-    public void ShowFileBrowser() {
-        Intent filepiIntent = new Intent(this, FilePickerActivity.class);
-        filepiIntent.putExtra(FilePickerActivity.REQUEST_CODE, FilePickerActivity.REQUEST_FILE);
-        startActivityForResult(filepiIntent, FilePickerActivity.REQUEST_FILE);
-    }
-
-    /**
-     Open dialog create new directory, get directory name
-     */
-    private EditText etNovyNazev;
-    @OnClick(R.id.btnPridatAdr)
-    public void CreateDir(){
-
-        final MaterialDialog dialog = new MaterialDialog.Builder(this)
-                .title(R.string.newName)
-                .customView(R.layout.rename_layout, false)
-                .positiveText(R.string.potvrdit)
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
-
-                        String strDirName = etNovyNazev.getText().toString();
-                        CreateNewDir(strDirName);
-
-
-                    }
-                })
-                .negativeText(R.string.cancel)
-                .onNegative(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
-                        materialDialog.dismiss();
-                    }
-                })
-                .build();
-
-
-        etNovyNazev = (EditText) dialog.getCustomView().findViewById(R.id.etNewName);
-        dialog.show();
-
-    }
-
-    /**
-     *Create new directory method
-     */
-    private void CreateNewDir(String strDirName) {
-        new AssyncFtpTaskActions().execute("CREATEDIR", strDirName);
-
-    }
-
-    /**
-     *Get uploading file's path
-     */
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        if (requestCode == FilePickerActivity.REQUEST_FILE
-                && resultCode == RESULT_OK) {
-
-            String filePath = data.
-                    getStringExtra(FilePickerActivity.FILE_EXTRA_DATA_PATH);
-            if (filePath != null) {
-                UploadFileToFtp(filePath);
-            }
-        }
-        super.onActivityResult(requestCode, resultCode, data);
-    }
-
-    /**
-     Upload file to ftp method
-     */
-    private void UploadFileToFtp(String filePath) {
-        new AssyncFtpTaskActions().execute("UPLOADFILE",filePath);
-    }
-
-    /**
-     Delete directory from ftp
-     */
-    public void DeleteDirFromFtp(String name) {
-        new AssyncFtpTaskActions().execute("DELETEDIR", name);
+    private void DownloadFile(String strDirName) {
+        new AssyncFtpTaskActions().execute("DOWNLOAD", strDirName);
     }
 
 
     /**
-     Assync task for ftp directory browsing
+     * Assync task for ftp directory browsing
      */
     public class AssyncFtpTask extends AsyncTask<String, Void, Integer> {
 
@@ -394,7 +165,7 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
                  Establish connection
                  */
                 mFTPClient.setControlEncoding("UTF-8");
-                mFTPClient.connect(FTP_HOST, 21);
+                mFTPClient.connect(FTP_HOST, 6888);
                 mFTPClient.login(FTP_USER, FTP_PASS);
                 mFTPClient.enterLocalPassiveMode();
                 mFTPClient.changeWorkingDirectory(strPath);
@@ -414,9 +185,13 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
                         break;
                     }
                     case "DOWNLOAD": {
-
-                        FileOutputStream fos = new FileOutputStream(chooserPath);
+                        File file = new File(Environment.getExternalStorageDirectory() + File.separator + "EWI");
+                        file.mkdir();
+                        File pdffile = new File(Environment.getExternalStorageDirectory() + File.separator + "EWI" + File.separator + params[1]);
+                        pdffile.createNewFile();
+                        FileOutputStream fos = new FileOutputStream(Environment.getExternalStorageDirectory() + File.separator + "EWI" + File.separator + params[1]);
                         mFTPClient.retrieveFile(strPath + "/" + params[1], fos);
+                        fos.flush();
                         fos.close();
                         break;
                     }
@@ -442,7 +217,7 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
                         break;
                     }
                     case "UPLOADFILE": {
-                        File f  = new File(params[1]);
+                        File f = new File(params[1]);
                         InputStream input = new FileInputStream(f);
                         if (mFTPClient.getReplyCode() != 550) {
                             runOnUiThread(new Runnable() {
@@ -482,7 +257,7 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
         }
 
         /**
-         Show changed files or fail message
+         * Show changed files or fail message
          */
 
         @Override
@@ -502,47 +277,5 @@ public class MainActivity extends AppCompatActivity implements FolderChooserDial
         }
     }
 
-    /**
-     Show dialog for  connection settings
-     */
-
-    private void ShowDialog() {
-        MaterialDialog dialog = new MaterialDialog.Builder(this)
-                .title(R.string.conn_config)
-                .customView(R.layout.dialog_layout, false)
-                .positiveText(R.string.connect)
-                .onPositive(new MaterialDialog.SingleButtonCallback() {
-                    @Override
-                    public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
-
-                        FTP_HOST = etAdr.getText().toString();
-                        FTP_USER = etNazev.getText().toString();
-                        FTP_PASS = etPw.getText().toString();
-
-                        SavePreffs();
-                        LoadIntoDirectory(strPath);
-
-                    }
-                }).build();
-
-        etNazev = (EditText) dialog.getCustomView().findViewById(R.id.dialNazev);
-        etNazev.setText(FTP_USER);
-        etPw = (EditText) dialog.getCustomView().findViewById(R.id.dialPw);
-        etPw.setText(FTP_PASS);
-        etAdr = (EditText) dialog.getCustomView().findViewById(R.id.dialAdr);
-        etAdr.setText(FTP_HOST);
-
-        dialog.show();
-    }
-
-    /***
-     save prefferences
-     */
-
-    private void SavePreffs() {
-        Prefs.putString("prefUSER", FTP_USER);
-        Prefs.putString("prefPass", FTP_PASS);
-        Prefs.putString("prefHost", FTP_HOST);
-    }
 
 }
--- app/src/main/java/baranek/vojtech/ftpclient/MyRecyclerAdapter.java
+++ app/src/main/java/baranek/vojtech/ftpclient/MyRecyclerAdapter.java
@@ -3,35 +3,28 @@ package baranek.vojtech.ftpclient;
 import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import com.afollestad.materialdialogs.DialogAction;
-import com.afollestad.materialdialogs.MaterialDialog;
-import com.afollestad.materialdialogs.folderselector.FolderChooserDialog;
-import com.kennyc.bottomsheet.BottomSheet;
-import com.kennyc.bottomsheet.BottomSheetListener;
-
 import org.apache.commons.net.ftp.FTPFile;
 
 /**
  * Created by Farmas on 17.10.2015.
- *
+ * <p/>
  * Custom adapter for RecylclerView for displaying files
  */
-public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.CustomViewHolder>  {
+public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.CustomViewHolder> {
 
     private int posicionie;
-    private  FTPFile[] files;
-    private Context c ;
-    private MainActivity mAct =null;
+    private FTPFile[] files;
+    private Context c;
+    private MainActivity mAct = null;
 
-    public MyRecyclerAdapter(Context c,  FTPFile[] files, MainActivity mAct) {
-        this.c=c;
+    public MyRecyclerAdapter(Context c, FTPFile[] files, MainActivity mAct) {
+        this.c = c;
         this.files = files;
         this.mAct = mAct;
 
@@ -39,9 +32,9 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
 
     @Override
     public MyRecyclerAdapter.CustomViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-       View View = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_row,null);
+        View View = LayoutInflater.from(parent.getContext()).inflate(R.layout.one_row, null);
         CustomViewHolder viewHolder = new CustomViewHolder(View);
-        return  viewHolder;
+        return viewHolder;
 
 
     }
@@ -54,40 +47,17 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
         if (files[position].isDirectory())
         /**
          * Is directory
-         */
-        {holder.imageView.setBackgroundResource(R.drawable.ic_folder_open_black_48dp);}
-        else{
+         */ {
+            holder.imageView.setBackgroundResource(R.mipmap.ic_ftp_launcher);
+        } else {
             /**
              * is file
              */
-            holder.imageView.setBackgroundResource(R.drawable.ic_insert_drive_file_black_48dp);
+            holder.imageView.setBackgroundResource(R.mipmap.ic_ftp_launcher);
         }
 
         holder.tv1.setText(String.valueOf(files[position].getName()));
 
-        /**
-         * Actions for item click
-         */
-        View.OnClickListener clickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if (files[position].isDirectory())
-                /**
-                 * Open folder
-                 */
-                {
-                   mAct.LoadIntoDirectory("/"+files[position].getName());
-                }
-                else
-                {
-                    /**
-                     * Show bottom sheet for file
-                     */
-                    OpenBottomSheetMenu(position);
-                }
-
-            }
-        };
         /**
          * Actions for long click on directory
          */
@@ -97,16 +67,13 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
                 if (files[position].isDirectory())
                 /**
                  * Open bottom sheet for directory
-                 */
-                {
+                 */ {
                     OpenBottomSheetMenuForDir(position);
                 }
                 return false;
             }
         };
 
-        holder.tv1.setOnClickListener(clickListener);
-        holder.imageView.setOnClickListener(clickListener);
 
         holder.tv1.setOnLongClickListener(longClickListener);
         holder.imageView.setOnLongClickListener(longClickListener);
@@ -125,49 +92,6 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
 
     private void OpenBottomSheetMenuForDir(int pos) {
         posicionie = pos;
-        new BottomSheet.Builder(mAct)
-                .setSheet(R.menu.bottomsheet_dirmenu)
-                .setTitle(R.string.podrobnosti)
-                .setListener(new BottomSheetListener() {
-                    @Override
-                    public void onSheetShown() {
-
-                    }
-
-                    @Override
-                    public void onSheetItemSelected(MenuItem menuItem) {
-
-                        switch (menuItem.getItemId()) {
-
-                            /**
-                             * Rename direcotry
-                             */
-                            case R.id.menRename: {
-                                ShowRenameDialog();
-                                break;
-
-                            }
-                            /**
-                             * Delete directory
-                             */
-                            case R.id.menSmazat: {
-                                mAct.DeleteDirFromFtp(files[posicionie].getName());
-                                break;
-                            }
-
-
-                        }
-                    }
-
-
-                    @Override
-                    public void onSheetDismissed(int i) {
-
-                    }
-                })
-                            .show();
-
-
     }
 
     /**
@@ -179,56 +103,6 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
     private void OpenBottomSheetMenu(int pos) {
 
         posicionie = pos;
-        new BottomSheet.Builder(mAct)
-                .setSheet(R.menu.bottomsheet_filemenu)
-                .setTitle(R.string.podrobnosti)
-                .setListener(new BottomSheetListener() {
-                    @Override
-                    public void onSheetShown() {
-
-                    }
-
-                    @Override
-                    public void onSheetItemSelected(MenuItem menuItem) {
-
-                        switch (menuItem.getItemId()) {
-
-                            /**
-                             * Download file, show folder chooser dialog
-                             */
-                            case R.id.menStahnout :{
-
-                                ShowDownloadDialog();
-                                mAct.DownloadFtpFile(files[posicionie].getName());
-
-                                break;
-
-                                /**
-                                 * Rename file
-                                 */
-                            }case R.id.menRename :{
-
-                                ShowRenameDialog();
-
-                                break;
-
-                                /**
-                                 * Delete file
-                                 */
-                            }case R.id.menSmazat :{
-
-                                mAct.DeleteFileFromFtp(files[posicionie].getName());
-                                break;
-                        }
-                }
-                    }
-
-                    @Override
-                    public void onSheetDismissed(int i) {
-
-                    }
-                })
-                .show();
 
 
     }
@@ -238,10 +112,6 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
      */
     private void ShowDownloadDialog() {
 
-        final FolderChooserDialog folderrCh = new FolderChooserDialog.Builder(mAct)
-                .chooseButton(R.string.potvrdit)  // changes label of the choose button
-                .initialPath("/sdcard/Download")  // changes initial path, defaults to external storage directory
-                .show();
     }
 
     private EditText etNovyNazev;
@@ -252,32 +122,6 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
      */
 
     private void ShowRenameDialog() {
-
-            final MaterialDialog dialog = new MaterialDialog.Builder(mAct)
-                    .title("Nový název")
-                    .customView(R.layout.rename_layout, false)
-                    .positiveText("Ok")
-                    .onPositive(new MaterialDialog.SingleButtonCallback() {
-                        @Override
-                        public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
-
-                            strNovyNazev = etNovyNazev.getText().toString();
-                            mAct.RenameFileFromFtp(files[posicionie].getName(),strNovyNazev);
-
-                        }
-                    })
-                    .negativeText("Zrušit")
-                    .onNegative(new MaterialDialog.SingleButtonCallback() {
-                        @Override
-                        public void onClick(MaterialDialog materialDialog, DialogAction dialogAction) {
-                           materialDialog.dismiss();
-                        }
-                    })
-                    .build();
-
-
-            etNovyNazev = (EditText) dialog.getCustomView().findViewById(R.id.etNewName);
-            dialog.show();
 
 
     }
@@ -293,15 +137,15 @@ public class MyRecyclerAdapter extends RecyclerView.Adapter<MyRecyclerAdapter.Cu
      * Custom view holder for row
      */
 
-    public class CustomViewHolder extends RecyclerView.ViewHolder{
+    public class CustomViewHolder extends RecyclerView.ViewHolder {
 
-        protected TextView tv1 ;
+        protected TextView tv1;
         protected ImageView imageView;
 
         public CustomViewHolder(View itemView) {
             super(itemView);
-            this.tv1 = (TextView)itemView.findViewById(R.id.title);
-            this.imageView = (ImageView)itemView.findViewById(R.id.type);
+            this.tv1 = (TextView) itemView.findViewById(R.id.title);
+            this.imageView = (ImageView) itemView.findViewById(R.id.type);
         }
     }
 
