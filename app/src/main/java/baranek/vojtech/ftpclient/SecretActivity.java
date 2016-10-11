d96d92c5b243b28cabaab491969d68ddb0ef4e61
kjh08490@ly.com <kjh08490@ly.com>
2016-10-11 08:36:40 +0800

    fix download

--- app/src/main/java/baranek/vojtech/ftpclient/SecretActivity.java
+++ app/src/main/java/baranek/vojtech/ftpclient/SecretActivity.java
@@ -2,20 +2,28 @@ package baranek.vojtech.ftpclient;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
 import baranek.vojtech.ftpclient.api.EwiService;
 import baranek.vojtech.ftpclient.api.Host;
-import baranek.vojtech.ftpclient.entity.TempResBody;
 import baranek.vojtech.ftpclient.entity.UpdaeResBody;
 import baranek.vojtech.ftpclient.entity.Update;
 import baranek.vojtech.ftpclient.entity.UpdateObj;
 import baranek.vojtech.ftpclient.gsonfactory.GsonConverterFactory;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -26,6 +34,7 @@ import retrofit2.Retrofit;
  */
 public class SecretActivity extends Activity implements View.OnClickListener {
 
+    private static final String TAG = "TAG";
 
     private EditText et_secret;
 
@@ -35,6 +44,9 @@ public class SecretActivity extends Activity implements View.OnClickListener {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secret);
+
+
+
         et_secret = (EditText) findViewById(R.id.et_secret);
         btn_sure = (Button) findViewById(R.id.btn_sure);
         btn_sure.setOnClickListener(this);
@@ -75,20 +87,102 @@ public class SecretActivity extends Activity implements View.OnClickListener {
                 response.body().d.Data.FileUrl = "http://gdown.baidu.com/data/wisegame/a6cb82227e027503/tongchenglvyou_114.apk";
                 if (response != null && response.body() != null && response.body().d != null && response.body().d.Data != null) {
                     Update update = response.body().d.Data;
-                    if(!TextUtils.isEmpty(update.FileUrl)){
+                    if (!TextUtils.isEmpty(update.FileUrl)) {
                         // 下载
-                        Toast.makeText(SecretActivity.this,"Downloding...",Toast.LENGTH_SHORT).show();
+                        Toast.makeText(SecretActivity.this, "Downloding...", Toast.LENGTH_SHORT).show();
 
+                        downlaod(response.body().d.Data.FileUrl);
                     }
-                }else{
-                    Toast.makeText(SecretActivity.this,"已经是最新版本！",Toast.LENGTH_SHORT).show();
+                } else {
+                    Toast.makeText(SecretActivity.this, "已经是最新版本！", Toast.LENGTH_SHORT).show();
                 }
             }
 
             @Override
             public void onFailure(Call<UpdaeResBody> call, Throwable throwable) {
-                Toast.makeText(SecretActivity.this,"更新接口异常，请联系维护人员 ！",Toast.LENGTH_SHORT).show();
+                Toast.makeText(SecretActivity.this, "更新接口异常，请联系维护人员 ！", Toast.LENGTH_SHORT).show();
             }
         });
+    }
+
+
+    private void downlaod(final String fileUrl){
+
+        new Thread(){
+            @Override
+            public void run() {
+                super.run();
+                Retrofit retrofit = new Retrofit.Builder()
+                        .baseUrl(Host.HOST)
+                        .addConverterFactory(GsonConverterFactory.create())
+                        .build();
+                EwiService service = retrofit.create(EwiService.class);
+                Call<ResponseBody> call = service.downloadFileWithDynamicUrlSync(fileUrl);
+                call.enqueue(new Callback<ResponseBody>() {
+                    @Override
+                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
+                        boolean writtenToDisk = writeResponseBodyToDisk(response.body());
+                    }
+
+                    @Override
+                    public void onFailure(Call<ResponseBody> call, Throwable throwable) {
+
+                    }
+                });
+            }
+        }.start();
+
+    }
+
+
+
+    private boolean writeResponseBodyToDisk(ResponseBody body) {
+        try {
+            // todo change the file location/name according to your needs
+            File futureStudioIconFile = new File(getExternalFilesDir(null) + File.separator + "Future Studio Icon.png");
+
+            InputStream inputStream = null;
+            OutputStream outputStream = null;
+
+            try {
+                byte[] fileReader = new byte[4096];
+
+                long fileSize = body.contentLength();
+                long fileSizeDownloaded = 0;
+
+                inputStream = body.byteStream();
+                outputStream = new FileOutputStream(futureStudioIconFile);
+
+                while (true) {
+                    int read = inputStream.read(fileReader);
+
+                    if (read == -1) {
+                        break;
+                    }
+
+                    outputStream.write(fileReader, 0, read);
+
+                    fileSizeDownloaded += read;
+
+                    Log.d(TAG, "file download: " + fileSizeDownloaded + " of " + fileSize);
+                }
+
+                outputStream.flush();
+
+                return true;
+            } catch (IOException e) {
+                return false;
+            } finally {
+                if (inputStream != null) {
+                    inputStream.close();
+                }
+
+                if (outputStream != null) {
+                    outputStream.close();
+                }
+            }
+        } catch (IOException e) {
+            return false;
+        }
     }
 }
--- app/src/main/java/baranek/vojtech/ftpclient/api/EwiService.java
+++ app/src/main/java/baranek/vojtech/ftpclient/api/EwiService.java
@@ -3,10 +3,13 @@ package baranek.vojtech.ftpclient.api;
 import baranek.vojtech.ftpclient.entity.EwiResBody;
 import baranek.vojtech.ftpclient.entity.TempResBody;
 import baranek.vojtech.ftpclient.entity.UpdaeResBody;
+import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
 import retrofit2.http.Query;
+import retrofit2.http.Streaming;
+import retrofit2.http.Url;
 
 /**
  * Created by kjh08490 on 2016/3/16.
@@ -35,4 +38,8 @@ public interface EwiService {
             @Query("Code") String code,
             @Query("Ver") String ver
     );
+
+    @Streaming
+    @GET
+    Call<ResponseBody> downloadFileWithDynamicUrlSync(@Url String fileUrl);
 }
