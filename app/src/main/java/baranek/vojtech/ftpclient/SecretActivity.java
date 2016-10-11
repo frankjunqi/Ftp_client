0a37044ba05ae5afbcce4ca7c7e5aaee672a319f
kjh08490 <kjh08490@ly.com>
2016-10-11 09:56:05 +0800

    bug fix_download

--- app/src/main/java/baranek/vojtech/ftpclient/SecretActivity.java
+++ app/src/main/java/baranek/vojtech/ftpclient/SecretActivity.java
@@ -1,21 +1,21 @@
 package baranek.vojtech.ftpclient;
 
 import android.app.Activity;
+import android.app.DownloadManager;
+import android.content.Context;
 import android.content.Intent;
-import android.os.AsyncTask;
+import android.database.ContentObserver;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.os.Handler;
+import android.os.Message;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
-
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 
 import baranek.vojtech.ftpclient.api.EwiService;
 import baranek.vojtech.ftpclient.api.Host;
@@ -23,7 +23,6 @@ import baranek.vojtech.ftpclient.entity.UpdaeResBody;
 import baranek.vojtech.ftpclient.entity.Update;
 import baranek.vojtech.ftpclient.entity.UpdateObj;
 import baranek.vojtech.ftpclient.gsonfactory.GsonConverterFactory;
-import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -40,12 +39,12 @@ public class SecretActivity extends Activity implements View.OnClickListener {
 
     private Button btn_sure, btn_update;
 
+    private ContentObserver contentObserver;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_secret);
-
-
 
         et_secret = (EditText) findViewById(R.id.et_secret);
         btn_sure = (Button) findViewById(R.id.btn_sure);
@@ -58,12 +57,12 @@ public class SecretActivity extends Activity implements View.OnClickListener {
     public void onClick(View v) {
         switch (v.getId()) {
             case R.id.btn_sure:
-                String secret = et_secret.getText().toString().replace(" ","");
-                if("mesmubea".equals(secret)){
-                    Toast.makeText(this,"登陆成功！",Toast.LENGTH_SHORT).show();
-                    startActivity(new Intent(this,SettingActivity.class));
-                }else{
-                    Toast.makeText(this,"密码错误，请联系管理员！",Toast.LENGTH_SHORT).show();
+                String secret = et_secret.getText().toString().replace(" ", "");
+                if ("mesmubea".equals(secret)) {
+                    Toast.makeText(this, "登陆成功！", Toast.LENGTH_SHORT).show();
+                    startActivity(new Intent(this, SettingActivity.class));
+                } else {
+                    Toast.makeText(this, "密码错误，请联系管理员！", Toast.LENGTH_SHORT).show();
                 }
                 break;
             case R.id.btn_update:
@@ -72,7 +71,7 @@ public class SecretActivity extends Activity implements View.OnClickListener {
         }
     }
 
-    public void requestUpdate(){
+    public void requestUpdate() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl(Host.UpdateHost)
                 .addConverterFactory(GsonConverterFactory.create())
@@ -84,14 +83,13 @@ public class SecretActivity extends Activity implements View.OnClickListener {
             public void onResponse(Call<UpdaeResBody> call, Response<UpdaeResBody> response) {
                 response.body().d = new UpdateObj();
                 response.body().d.Data = new Update();
-                response.body().d.Data.FileUrl = "http://gdown.baidu.com/data/wisegame/a6cb82227e027503/tongchenglvyou_114.apk";
+                response.body().d.Data.FileUrl = "http://gdown.baidu.com/data/wisegame/dc1095dc5fe68e29/qianchengwuyou51Job_610.apk";
                 if (response != null && response.body() != null && response.body().d != null && response.body().d.Data != null) {
                     Update update = response.body().d.Data;
                     if (!TextUtils.isEmpty(update.FileUrl)) {
                         // 下载
                         Toast.makeText(SecretActivity.this, "Downloding...", Toast.LENGTH_SHORT).show();
-
-                        downlaod(response.body().d.Data.FileUrl);
+                        downloadApk(response.body().d.Data.FileUrl);
                     }
                 } else {
                     Toast.makeText(SecretActivity.this, "已经是最新版本！", Toast.LENGTH_SHORT).show();
@@ -105,84 +103,119 @@ public class SecretActivity extends Activity implements View.OnClickListener {
         });
     }
 
-
-    private void downlaod(final String fileUrl){
-
-        new Thread(){
-            @Override
-            public void run() {
-                super.run();
-                Retrofit retrofit = new Retrofit.Builder()
-                        .baseUrl(Host.HOST)
-                        .addConverterFactory(GsonConverterFactory.create())
-                        .build();
-                EwiService service = retrofit.create(EwiService.class);
-                Call<ResponseBody> call = service.downloadFileWithDynamicUrlSync(fileUrl);
-                call.enqueue(new Callback<ResponseBody>() {
-                    @Override
-                    public void onResponse(Call<ResponseBody> call, Response<ResponseBody> response) {
-                        boolean writtenToDisk = writeResponseBodyToDisk(response.body());
-                    }
-
-                    @Override
-                    public void onFailure(Call<ResponseBody> call, Throwable throwable) {
-
-                    }
-                });
-            }
-        }.start();
-
-    }
-
-
-
-    private boolean writeResponseBodyToDisk(ResponseBody body) {
+    private void downloadApk(String apkUrl) {
+        btn_update.setEnabled(false);
+        // 下载
         try {
-            // todo change the file location/name according to your needs
-            File futureStudioIconFile = new File(getExternalFilesDir(null) + File.separator + "Future Studio Icon.png");
-
-            InputStream inputStream = null;
-            OutputStream outputStream = null;
-
+            final DownloadManager downloadManager = (DownloadManager) getSystemService(Context.DOWNLOAD_SERVICE);
+            clearDownLoadApk(downloadManager);
+            Uri uri = Uri.parse(apkUrl);
+            DownloadManager.Request request = new DownloadManager.Request(uri);
+            request.setTitle("EWI");
+            // 指定文件保存在应用的私有目录
+            request.setDestinationInExternalFilesDir(this, Environment.DIRECTORY_DOWNLOADS, TCTInsatllActionBroadcastReceiver.APK_FILE_NAME);
+            long reference = 0;
             try {
-                byte[] fileReader = new byte[4096];
-
-                long fileSize = body.contentLength();
-                long fileSizeDownloaded = 0;
-
-                inputStream = body.byteStream();
-                outputStream = new FileOutputStream(futureStudioIconFile);
-
-                while (true) {
-                    int read = inputStream.read(fileReader);
-
-                    if (read == -1) {
-                        break;
-                    }
-
-                    outputStream.write(fileReader, 0, read);
-
-                    fileSizeDownloaded += read;
-
-                    Log.d(TAG, "file download: " + fileSizeDownloaded + " of " + fileSize);
-                }
-
-                outputStream.flush();
-
-                return true;
-            } catch (IOException e) {
-                return false;
-            } finally {
-                if (inputStream != null) {
-                    inputStream.close();
-                }
-
-                if (outputStream != null) {
-                    outputStream.close();
-                }
+                reference = downloadManager.enqueue(request);// 下载id
+            } catch (IllegalArgumentException e) {
+                return;
             }
-        } catch (IOException e) {
-            return false;
+            final long id = reference;
+            // 注册数据库监听
+            getContentResolver().registerContentObserver(
+                    Uri.parse("content://downloads/my_downloads"),
+                    true,
+                    contentObserver = new ContentObserver(null) {
+                        @Override
+                        public void onChange(boolean selfChange) {
+                            DownloadManager.Query query = new DownloadManager.Query();
+                            query.setFilterById(id);
+                            Cursor my = downloadManager.query(query);
+                            if (my != null) {
+                                if (my.moveToFirst()) {
+                                    // String
+                                    // fileUri=my.getString(my.getColumnIndex(DownloadManager.COLUMN_LOCAL_URI));
+                                    int fileSize = my.getInt(my
+                                            .getColumnIndex(DownloadManager.COLUMN_TOTAL_SIZE_BYTES));
+                                    int bytesDL = my.getInt(my
+                                            .getColumnIndex(DownloadManager.COLUMN_BYTES_DOWNLOADED_SO_FAR));
+                                    int percent = bytesDL * 100 / fileSize;
+                                    updateProgress(percent);
+
+                                    if (percent >= 99) {
+                                        btn_update.setEnabled(true);
+                                    }
+
+                                }
+                                my.close();
+                            } else {
+                            }
+                        }
+                    });
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
+
+    /**
+     * 删除之前下载的apk文件
+     */
+    private void clearDownLoadApk(final DownloadManager manager) {
+        // 删除失败和成功状态的数据
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                int delete[] = {DownloadManager.STATUS_FAILED, DownloadManager.STATUS_SUCCESSFUL};
+                DownloadManager.Query query = new DownloadManager.Query();
+                for (int element : delete) {
+                    query.setFilterByStatus(element);
+                    Cursor cursor = manager.query(query);
+                    if (cursor != null) {
+                        if (cursor.moveToFirst()) {
+                            do {
+                                manager.remove(cursor.getLong(cursor.getColumnIndex(DownloadManager.COLUMN_ID)));
+                            } while (cursor.moveToNext());
+                        }
+                        cursor.close();
+                    }
+                }
+            }
+        }).start();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (contentObserver != null) {
+            getContentResolver().unregisterContentObserver(contentObserver);
+        }
+    }
+
+    /**
+     * 版本升级更新进度
+     *
+     * @param percent
+     */
+    protected void updateProgress(int percent) {
+        Message message = handler.obtainMessage();
+        message.what = 2;
+        message.arg1 = percent;
+        handler.sendMessage(message);
+    }
+
+    public Handler handler = new Handler() {
+        @Override
+        public void handleMessage(Message msg) {
+            super.handleMessage(msg);
+            switch (msg.what) {
+                case 2:
+                    if (msg.arg1 == 100) {
+                        btn_update.setText("升级");
+                    } else {
+                        btn_update.setText("下载进度： " + String.valueOf(msg.arg1) + "%");
+                    }
+                    break;
+            }
+        }
+    };
 }
